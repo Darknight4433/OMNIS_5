0cdd66e3628819408731f7d4aeb722297080b2cf
Vaishnavi L <vaishnavil4433@gmail.com>
2026-01-20 11:56:01 +0530

    Update secrets template with ElevenLabs keys

--- secrets_template_new.py
+++ secrets_template_new.py
@@ -1,6 +1,7 @@
 
 # ElevenLabs API Keys for rotation
 ELEVENLABS_KEYS = [
-    '', # Key 1
-    '', # Key 2
+    'sk_1671a85ae149df5c19809c2df4d439f747d9fe181617e82a',
+    'sk_d508cc204cfca17e6877698aff7a8908959cd50a91523da1',
+    'sk_cf261d99ce4ddd58372efd9da1e2ac358b8ae755d8eb18d6'
 ]
